7def41393c1e1160595f41803f6a5a82dbda2b40
pvillela <pvillela@gmail.com>
2025-03-20 23:48:11 -0400

    Tweaked run names in bench.sh.

--- bench.sh
+++ bench.sh
@@ -27,13 +27,13 @@ RUSTFLAGS="-Awarnings" \
 SCALE_NAME="micros_scale" \
 FN_NAME_PAIRS="base_median_no_var/hi_1pct_median_no_var" \
 VERBOSE="false" \
-cargo bench --bench bench_t --all-features --target-dir target/bench-target -- $1 base-no-var/hi-no-var
+cargo bench --bench bench_t --all-features --target-dir target/bench-target -- $1 base-no-var/hi-1pct-no-var
 
 RUSTFLAGS="-Awarnings" \
 SCALE_NAME="micros_scale" \
 FN_NAME_PAIRS="hi_1pct_median_no_var/base_median_no_var" \
 VERBOSE="false" \
-cargo bench --bench bench_t --all-features --target-dir target/bench-target -- $1 hi-no-var/base-no-var
+cargo bench --bench bench_t --all-features --target-dir target/bench-target -- $1 hi-1pct-no-var/base-no-var
 
 RUSTFLAGS="-Awarnings" \
 SCALE_NAME="micros_scale" \

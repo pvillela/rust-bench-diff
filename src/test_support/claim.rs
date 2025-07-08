bfda833fe75f1d5f14cee2d2a96c58fa6dbc92f0
pvillela <pvillela@gmail.com>
2025-07-07 22:20:37 -0400

    Refactored test_support::claim module, removing FnSpecPair.

--- src/test_support/claim.rs
+++ src/test_support/claim.rs
@@ -42,99 +42,119 @@ fn check_hyp_test_result(res: HypTestResult, hyp: Hyp) -> Option<String> {
     }
 }
 
-struct FnSpecPair {
-    spec_f1: FnSpec,
-    spec_f2: FnSpec,
+#[inline(always)]
+fn cmp_hyp(median_ratio: f64) -> Hyp {
+    match median_ratio.partial_cmp(&1.0) {
+        Some(Ordering::Less) => Some(AltHyp::Lt),
+        Some(Ordering::Equal) => None,
+        Some(Ordering::Greater) => Some(AltHyp::Gt),
+        None => panic!("invalid spec_f1 or spec_f2"),
+    }
 }
 
-impl FnSpecPair {
-    #[inline(always)]
-    fn hyp(&self) -> Hyp {
-        match self
-            .spec_f1
-            .base_median_factor
-            .partial_cmp(&self.spec_f2.base_median_factor)
-        {
-            Some(Ordering::Less) => Some(AltHyp::Lt),
-            Some(Ordering::Equal) => None,
-            Some(Ordering::Greater) => Some(AltHyp::Gt),
-            None => panic!("invalid spec_f1 or spec_f2"),
-        }
-    }
+#[derive(Debug)]
+pub struct ClaimResult {
+    spec_f1: FnSpec,
+    spec_f2: FnSpec,
+    claim_name: String,
+    result: Option<String>,
+}
 
-    #[inline(always)]
-    fn target(&self) -> f64 {
-        self.spec_f1.base_median_factor / self.spec_f2.base_median_factor
-    }
-
-    fn welch_ratio_test(&self, out: &DiffOut, alpha: f64) -> ClaimResult {
+impl ClaimResult {
+    fn welch_ratio_test(
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
+        out: &DiffOut,
+        alpha: f64,
+    ) -> ClaimResult {
         let claim_name = "welch_ratio_test";
-        let hyp = self.hyp();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
+        let hyp = cmp_hyp(ratio);
         let result = {
             let res = out.welch_ln_test(0., alt_hyp(hyp), alpha);
             check_hyp_test_result(res, hyp)
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
-    fn student_diff_test(&self, out: &DiffOut, alpha: f64) -> ClaimResult {
+    fn student_diff_test(
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
+        out: &DiffOut,
+        alpha: f64,
+    ) -> ClaimResult {
         let claim_name = "student_diff_test";
-        let hyp = self.hyp();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
+        let hyp = cmp_hyp(ratio);
         let result = {
             let res = out.student_diff_test(alt_hyp(hyp), alpha);
             check_hyp_test_result(res, hyp)
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
-    fn student_ratio_test(&self, out: &DiffOut, alpha: f64) -> ClaimResult {
+    fn student_ratio_test(
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
+        out: &DiffOut,
+        alpha: f64,
+    ) -> ClaimResult {
         let claim_name = "student_ratio_test";
-        let hyp = self.hyp();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
+        let hyp = cmp_hyp(ratio);
         let result = {
             let res = out.student_diff_ln_test(alt_hyp(hyp), alpha);
             check_hyp_test_result(res, hyp)
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
-    fn ratio_medians_f1_f2_near_target(&self, out: &DiffOut) -> ClaimResult {
+    fn ratio_medians_f1_f2_near_target(
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
+        out: &DiffOut,
+    ) -> ClaimResult {
         let claim_name = "ratio_medians_f1_f2_near_target";
-        let target = self.target();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
         let result = {
             let ratio_medians_f1_f2 = out.ratio_medians_f1_f2();
 
-            if ratio_medians_f1_f2.approx_eq(target, 0.005) {
+            if ratio_medians_f1_f2.approx_eq(ratio, 0.005) {
                 None
             } else {
                 Some(format!(
-                    "ratio_medians_f1_f2={ratio_medians_f1_f2}, target={target}"
+                    "ratio_medians_f1_f2={ratio_medians_f1_f2}, target={ratio}"
                 ))
             }
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
-    fn ratio_medians_f1_f2_near_ratio_from_lns(&self, out: &DiffOut) -> ClaimResult {
+    fn ratio_medians_f1_f2_near_ratio_from_lns(
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
+        out: &DiffOut,
+    ) -> ClaimResult {
         let claim_name = "ratio_medians_f1_f2_near_ratio_from_lns";
         let result = {
             let ratio_medians_f1_f2 = out.ratio_medians_f1_f2();
@@ -149,102 +169,103 @@ impl FnSpecPair {
             }
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
     fn target_ratio_medians_f1_f2_in_welch_ratio_ci(
-        &self,
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
         out: &DiffOut,
         alpha: f64,
     ) -> ClaimResult {
         let claim_name = "target_ratio_medians_f1_f2_in_welch_ratio_ci";
-        let target = self.target();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
         let result = {
             let ci = out.welch_ratio_ci(alpha);
 
-            if ci.position_of(target) == PositionWrtCi::In {
+            if ci.position_of(ratio) == PositionWrtCi::In {
                 None
             } else {
                 Some(format!(
-                    "ratio_medians_f1_f2={target}, welch_ratio_ci={ci:?}"
+                    "ratio_medians_f1_f2={ratio}, welch_ratio_ci={ci:?}"
                 ))
             }
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
     fn target_ratio_medians_f1_f2_in_student_ratio_ci(
-        &self,
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
         out: &DiffOut,
         alpha: f64,
     ) -> ClaimResult {
         let claim_name = "target_ratio_medians_f1_f2_in_student_ratio_ci";
-        let target = self.target();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
         let result = {
             let ci = out.student_ratio_ci(alpha);
 
-            if ci.position_of(target) == PositionWrtCi::In {
+            if ci.position_of(ratio) == PositionWrtCi::In {
                 None
             } else {
                 Some(format!(
-                    "ratio_medians_f1_f2={target}, student_ratio_ci={ci:?}"
+                    "ratio_medians_f1_f2={ratio}, student_ratio_ci={ci:?}"
                 ))
             }
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
-    fn wilcoxon_rank_sum_test(&self, out: &DiffOut, alpha: f64) -> ClaimResult {
+    fn wilcoxon_rank_sum_test(
+        spec_f1: FnSpec,
+        spec_f2: FnSpec,
+        out: &DiffOut,
+        alpha: f64,
+    ) -> ClaimResult {
         let claim_name = "wilcoxon_rank_sum_test";
-        let hyp = self.hyp();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
+        let hyp = cmp_hyp(ratio);
         let result = {
             let res = out.wilcoxon_rank_sum_test(alt_hyp(hyp), alpha);
             check_hyp_test_result(res, hyp)
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
 
-    fn binomial_test(&self, out: &DiffOut, alpha: f64) -> ClaimResult {
+    fn binomial_test(spec_f1: FnSpec, spec_f2: FnSpec, out: &DiffOut, alpha: f64) -> ClaimResult {
         let claim_name = "binomial_test";
-        let hyp = self.hyp();
+        let ratio = spec_f1.base_median_factor / spec_f2.base_median_factor;
+        let hyp = cmp_hyp(ratio);
         let result = {
             let res = out.exact_binomial_f1_gt_f2_eq_half_test(alt_hyp(hyp), alpha);
             check_hyp_test_result(res, hyp)
         };
         ClaimResult {
-            spec_f1: self.spec_f1,
-            spec_f2: self.spec_f2,
+            spec_f1,
+            spec_f2,
             claim_name: claim_name.into(),
             result,
         }
     }
-}
-
-#[derive(Debug)]
-pub struct ClaimResult {
-    spec_f1: FnSpec,
-    spec_f2: FnSpec,
-    claim_name: String,
-    result: Option<String>,
 }
 
 pub struct ClaimResults {
@@ -300,26 +321,44 @@ impl ClaimResults {
         out: &DiffOut,
         verbose: bool,
     ) {
-        let spec_pair = FnSpecPair { spec_f1, spec_f2 };
-
-        self.push_claim_result(spec_pair.welch_ratio_test(out, alpha), verbose);
-        self.push_claim_result(spec_pair.student_diff_test(out, alpha), verbose);
-        self.push_claim_result(spec_pair.student_ratio_test(out, alpha), verbose);
-        self.push_claim_result(spec_pair.ratio_medians_f1_f2_near_target(out), verbose);
         self.push_claim_result(
-            spec_pair.ratio_medians_f1_f2_near_ratio_from_lns(out),
+            ClaimResult::welch_ratio_test(spec_f1, spec_f2, out, alpha),
             verbose,
         );
         self.push_claim_result(
-            spec_pair.target_ratio_medians_f1_f2_in_welch_ratio_ci(out, alpha),
+            ClaimResult::student_diff_test(spec_f1, spec_f2, out, alpha),
             verbose,
         );
         self.push_claim_result(
-            spec_pair.target_ratio_medians_f1_f2_in_student_ratio_ci(out, alpha),
+            ClaimResult::student_ratio_test(spec_f1, spec_f2, out, alpha),
             verbose,
         );
-        self.push_claim_result(spec_pair.wilcoxon_rank_sum_test(out, alpha), verbose);
-        self.push_claim_result(spec_pair.binomial_test(out, alpha), verbose);
+        self.push_claim_result(
+            ClaimResult::ratio_medians_f1_f2_near_target(spec_f1, spec_f2, out),
+            verbose,
+        );
+        self.push_claim_result(
+            ClaimResult::ratio_medians_f1_f2_near_ratio_from_lns(spec_f1, spec_f2, out),
+            verbose,
+        );
+        self.push_claim_result(
+            ClaimResult::target_ratio_medians_f1_f2_in_welch_ratio_ci(spec_f1, spec_f2, out, alpha),
+            verbose,
+        );
+        self.push_claim_result(
+            ClaimResult::target_ratio_medians_f1_f2_in_student_ratio_ci(
+                spec_f1, spec_f2, out, alpha,
+            ),
+            verbose,
+        );
+        self.push_claim_result(
+            ClaimResult::wilcoxon_rank_sum_test(spec_f1, spec_f2, out, alpha),
+            verbose,
+        );
+        self.push_claim_result(
+            ClaimResult::binomial_test(spec_f1, spec_f2, out, alpha),
+            verbose,
+        );
     }
 
     pub fn summary(&self) -> &BTreeMap<((FnSpec, FnSpec), String), u32> {

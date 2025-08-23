c264caf3d085c2b87231e4623b5c5f29a497ecf9
pvillela <pvillela@gmail.com>
2025-08-23 17:28:56 -0400

    Added 2 more bench scripts.

---
+++ bench-micros-both-lovar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export FN_NAME_PAIRS="1@lovar/1@lovar | 1.01@lovar/1@lovar | 1.05@lovar/1@lovar | 1.1@lovar/1@lovar | 1.25@lovar/1@lovar"
+
+BENCH_MODE=diff ./bench-micros.sh $1 "both-lovar:diff"
+
+BENCH_MODE=comp ./bench-micros.sh $1 "both-lovar:comp"
---
+++ bench-micros-diff-novar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FN_NAME_PAIRS="1@novar/1@novar | 1.01@novar/1@novar | 1.05@novar/1@novar | 1.1@novar/1@novar | 1.25@novar/1@novar"
+
+BENCH_MODE=diff ./bench-micros.sh $1 "diff-only-novar"

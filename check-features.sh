3c2264b2c2314987cd488bb86a086f2469544345
pvillela <pvillela@gmail.com>
2026-05-14 22:55:31 -0400

    Improved check-features.sh and test.sh.

--- check-features.sh
+++ check-features.sh
@@ -2,23 +2,21 @@
 
 set -e  # Stop script immediately on any error
 
-### With default features
-
-## Externally exposed feature combinations
+### With default features: Externally exposed feature combinations
 
 echo "***** (default feature)"
 cargo check --lib --tests --target-dir target/test-target
 
-## All targets and features
+### All targets and features
 
 echo "***** --all-targets --all-features"
 cargo check --all-targets --all-features --target-dir target/test-target
 
-## Benches
+### Benches
 
 echo "***** --features _bench"
 cargo check --lib --tests --benches --features _bench --target-dir target/test-target
 
-### Without default features
+### Without default features: Externally exposed feature combinations
 
 # Can't run without default features.
